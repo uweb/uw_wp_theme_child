32518ee4a41f3fdd5018ed9b4b66a14b58a82f1c
Lauren V Caple <lcaple@uw.edu>
2021-11-01 14:25:08 -0700

    fix: adjusted the enqueuing for stylesheets to come after all other parent theme items

--- functions.php
+++ functions.php
@@ -3,24 +3,15 @@
  * UW WP Theme Child Theme.
  */
 
-add_action( 'wp_enqueue_scripts', 'uw_child_enqueue_styles' );
+ // enqueue child childsheet, require the uw_wp_theme bootstrap style sheet
+ 
+add_action( 'wp_enqueue_scripts', 'uw_child_enqueue_styles', 11 );
+
 function uw_child_enqueue_styles() {
-	$parenthandle = 'uw_wp_theme-base-style';
-	$theme        = wp_get_theme();
-
-	// enqueue the UW WP Theme's styles.
-	wp_enqueue_style(
-		$parenthandle,
-		get_template_directory_uri() . '/style.css',
-		array(),
-		$theme->parent()->get( 'Version' )
-	);
-
-	// enqueue the child theme's styles.
-	wp_enqueue_style(
-		'uw-child-style',
-		get_stylesheet_uri(),
-		array( $parenthandle ),
-		$theme->get( 'Version' )
-	);
+	$parenthandle = 'uw_wp_theme-bootstrap';
+	
+	wp_enqueue_style( 'uw_wp_theme-child-style', get_stylesheet_uri(),
+        array( $parenthandle ), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
 }
